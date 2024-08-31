c605d47348d05fb3d353250175b0bd851b7a003b
irfan <85334710+irfan-007@users.noreply.github.com>
2024-08-31 20:18:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+let promise1=new Promise((resolved,rejected)=>{
+	let time=Math.floor(Math.random()*3)+1;
+	setTimeout(()=>resolved(time),time);
+});
+let promise2=new Promise((resolved,rejected)=>{
+	let time=Math.floor(Math.random()*3)+1;
+	setTimeout(()=>resolved(time),time);
+});
+let promise3=new Promise((resolved,rejected)=>{
+	let time=Math.floor(Math.random()*3)+1;
+	setTimeout(()=>resolved(time),time);
+});
+const arr=[promise1,promise2,promise3];
+const tbody=document.getElementById("output");
+tbody.innerHTML="<tr><td colspan=2>Loading...</td></tr>"
+let promiseCombi=Promise.all(arr);
+promiseCombi.then((values)=>{
+	console.log("success");
+	for(let i=1;i<=3;i++){
+		let promise="promise"+i;
+	}
+});
